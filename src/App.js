840f4a3fc54378c749de095748f5a442757c15aa
mtewatia21 <81717432+mtewatia21@users.noreply.github.com>
2023-08-01 23:25:12 -0700

    Added Functionality to remove a completed task

--- src/App.js
+++ src/App.js
@@ -33,11 +33,32 @@ function App() {
     });
   }
 
+  
+  const completed = tasks.filter(t=>t.done).length;
+  const totalTasks = tasks.length;
+  function getMessage(){
+    const percent = completed/totalTasks*100;
+    if(percent === 0)
+      return 'Start working ASAP!';
+    if(percent === 100)
+      return 'Amazing! All tasks done!';
+    return 'Keep at it!'
+  }
+
+  function deleteTask(taskIndex){
+    setTasks(prev => {
+      return prev.filter((taskObject, index) => index !== taskIndex)
+    })
+  }
   return (
     <main>
+      <h1>{completed}/{totalTasks} Complete</h1>
+      <h2>{getMessage()}</h2>
       <TaskForm onAdding = {addTask} />
       {tasks.map((task, idx) => (
-        <Task {...task} onToggle={done => updateTask(idx, done)} />
+        <Task {...task} 
+        onDelete={() => deleteTask(idx)}
+        onToggle={done => updateTask(idx, done)} />
       ))}
     </main>
   );
